3f353f151c4c5e9813b26514bf2e6b4b9465b02a
numbmn2n <57528037+SupBr1ght@users.noreply.github.com>
2025-07-25 15:50:49 +0300

    Update sanitiseInput.ts

--- backend/src/utils/sanitiseInput.ts
+++ backend/src/utils/sanitiseInput.ts
@@ -8,8 +8,6 @@ export function sanitizeHTML(input: string): string {
   return DOMPurify.sanitize(input, {
     ALLOWED_TAGS: ['a', 'code', 'i', 'strong'],
     ALLOWED_ATTR: ['href', 'title'],
-    // щоб не додавало зайвих тегів
     RETURN_DOM: false,
-    // Якщо хочеш, можна додати FORBID_TAGS, FORBID_ATTR
   });
 }
